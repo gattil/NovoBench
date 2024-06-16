9be63f910ab6ab0f4a38d133bd6476e6add7365e
jingbo02 <2420199773@qq.com>
2024-06-16 20:03:21 +0800

    test.py

--- tests/instanovo.py
+++ tests/instanovo.py
@@ -1,34 +1,55 @@
 import sys
 import os
-import yaml
-import pdb
-# os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+import csv
 sys.path.append("/jingbo/PyNovo/")
 
-from pynovo.models.instanovo.instanovo_runner import InstanovoRunner
-from pynovo.datasets import CustomDataset
+from pynovo.datasets import CustomDataset, NineSpeciesDataset
+from pynovo.models.instanovo import CasanovoRunner, CasanovoConfig
+
 
 file_mapping = {
-    "train" : "sample_train.parquet",
-    "valid" : "sample_test.parquet",
+    "train" : "train.parquet",
+    "valid" : "valid.parquet",
 }
 
 def train():
-    config_path = "/jingbo/PyNovo/pynovo/models/instanovo/instanovo_config.yaml"
-    with open(config_path) as f_in:
-        instanovo_config = yaml.safe_load(f_in)
-    dataset = CustomDataset("/usr/commondata/public/jingbo/nine_species/", file_mapping)
-    data = dataset.load_data(transform = InstanovoRunner.preprocessing_pipeline())
-    model = InstanovoRunner(instanovo_config)
-    model.train(data.get_train(), data.get_valid()) 
+    dataset = CustomDataset("/usr/commondata/public/jingbo/seven_species/", file_mapping)
+    data = dataset.load_data(transform = CasanovoRunner.preprocessing_pipeline())
+    config = CasanovoConfig()
+    model = CasanovoRunner(config)
+    model.train(data.get_train(), data.get_valid())
 
 
-def eval():
-    model_path = '/jingbo/PyNovo/pynovo/save_models/instanovo/instanovo_nine/epoch=28-step=450000.ckpt'
-    model = InstanovoRunner()
-    dataset = CustomDataset("/jingbo/PyNovo/data", file_mapping)
-    data = dataset.load_data(transform = InstanovoRunner.preprocessing_pipeline())
-    model.evaluate(data.get_valid(), model_path)
+train()
 
-eval()
-# train()
+
+def eval(data_dir, model_file, saved_path):
+    # dataset = CustomDataset("/usr/commondata/public/jingbo/nine_species/", file_mapping)
+    # data = dataset.load_data(transform = CasanovoRunner.preprocessing_pipeline())
+    # config = CasanovoConfig()
+    # model = CasanovoRunner(config,"/usr/commondata/local_public/jingbo/casanovo/nine_species/epoch=19-step=300000.ckpt")
+    # model.evaluate(data.get_valid())
+    if os.path.exists(saved_path):
+        base, extension = os.path.splitext(saved_path)
+        counter = 1
+        saved_path = f"{base}_{counter}{extension}"
+        while os.path.exists(saved_path):
+            counter += 1
+            saved_path = f"{base}_{counter}{extension}"
+        print(f"Saved file exist, use {saved_path} instead!")   
+    with open(saved_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['peptides_true', 'peptides_pred', 'peptides_score'])
+    
+    file_mapping = {"valid" : "test.parquet",}
+    dataset = CustomDataset(data_dir, file_mapping)
+    data = dataset.load_data(transform = CasanovoRunner.preprocessing_pipeline())
+    config = CasanovoConfig()
+    config.calculate_precision = True
+    
+    model = CasanovoRunner(config, model_file, saved_path)
+    model.evaluate(data.get_valid())
+
+
+
+        
